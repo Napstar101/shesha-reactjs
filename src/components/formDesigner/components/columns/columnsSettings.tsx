e90e6f0a4de5078573ca2b792069e28a4d43eafe
Phil Mashiane <phillemonm@boxfusion.co.za>
2021-07-12 16:25:17 +0200

    Added gutter to the columns components

--- src/components/formDesigner/components/columns/columnsSettings.tsx
+++ src/components/formDesigner/components/columns/columnsSettings.tsx
@@ -1,5 +1,5 @@
-import { Form, Input, FormInstance } from 'antd';
 import React from 'react';
+import { Form, Input, FormInstance, InputNumber } from 'antd';
 import { IColumnsComponentProps } from './columns';
 import ColumnsList from './columnsList';
 
@@ -17,13 +17,23 @@ function ColumnsSettings({ onSave, model, onValuesChange, form: providedForm }: 
   return (
     <Form form={form} onFinish={onSave} layout="vertical" onValuesChange={onValuesChange}>
       <Form.Item name="name" label="Name" rules={[{ required: true }]} initialValue={model?.name}>
-        <Input></Input>
+        <Input />
       </Form.Item>
+
+      <Form.Item name="gutterX" label="Gutter X" initialValue={model?.gutterX}>
+        <InputNumber min={1} max={48} step={4} />
+      </Form.Item>
+
+      <Form.Item name="gutterY" label="Gutter Y" initialValue={model?.gutterX}>
+        <InputNumber min={1} max={48} step={4} />
+      </Form.Item>
+
       <Form.Item name="label" label="Label" initialValue={model?.label}>
-        <Input></Input>
+       <Input />
       </Form.Item>
+
       <Form.Item name="columns" label="Columns" initialValue={model?.columns || []}>
-        <ColumnsList></ColumnsList>
+        <ColumnsList />
       </Form.Item>
     </Form>
   );
