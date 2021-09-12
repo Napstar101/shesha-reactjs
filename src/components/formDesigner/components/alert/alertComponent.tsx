222848e5068577a1895162fccde9cb17dc203173
Phil Mashiane <phillemonm@boxfusion.co.za>
2021-09-13 00:27:19 +0200

    Formdesigner Alert: Added description and showIcon properties to the component

--- src/components/formDesigner/components/alert/alertComponent.tsx
+++ src/components/formDesigner/components/alert/alertComponent.tsx
@@ -9,6 +9,8 @@ import { useForm } from '../../../../providers';
 
 export interface IAlertProps extends IConfigurableFormComponent {
   text: string;
+  description?: string;
+  showIcon?: boolean;
   alertType?: 'success' | 'info' | 'warning' | 'error';
 }
 
@@ -20,13 +22,21 @@ const AlertComponent: IToolboxComponent<IAlertProps> = {
   icon: <ExclamationCircleOutlined />,
   factory: (model: IConfigurableFormComponent) => {
     const { formMode, visibleComponentIds } = useForm();
-    const { text, alertType } = model as IAlertProps;
+    const { text, alertType, description, showIcon } = model as IAlertProps;
 
     const hiddenByCondition = visibleComponentIds && !visibleComponentIds.includes(model.id);
     const isHidden = formMode !== 'designer' && (model.hidden || hiddenByCondition);
     if (isHidden) return null;
 
-    return <Alert message={text} type={alertType} />;
+    return (
+      <Alert
+        message={text}
+        type={alertType}
+        description={description}
+        showIcon={showIcon}
+        style={{ marginBottom: 12 }} // Temporary. Make it configurable
+      />
+    );
   },
   settingsFormMarkup: settingsForm,
   validateSettings: model => validateConfigurableComponentSettings(settingsForm, model),
