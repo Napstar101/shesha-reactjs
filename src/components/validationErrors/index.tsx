54960d3bf04cabc4f5f3b22222dae3be6b6d6d96
Phil Mashiane <phillemonm@boxfusion.co.za>
2021-10-12 19:57:09 +0200

    Rendered ValidationErrors properly

--- src/components/validationErrors/index.tsx
+++ src/components/validationErrors/index.tsx
@@ -1,8 +1,7 @@
 import React, { FC } from 'react';
-import { Alert, AlertProps, notification } from 'antd';
+import { Alert, AlertProps } from 'antd';
 import { IErrorInfo } from '../../interfaces/errorInfo';
 import { IAjaxResponseBase } from '../../interfaces/ajaxResponse';
-import _ from 'lodash';
 
 export interface IValidationErrorsProps {
   error: IAjaxResponseBase | IErrorInfo | string | boolean;
@@ -10,18 +9,15 @@ export interface IValidationErrorsProps {
 
 const DEFAULT_ERROR_MSG = 'Sorry, an error has occurred. Please try again later';
 
-const renderAlert = (props: AlertProps) => {
-  notification.error({ message: props.message });
-  return <Alert className="sha-validation-error-alert" type="error" showIcon {...props} />;
-};
-
 /**
  * A component for displaying validation errors
  */
 export const ValidationErrors: FC<IValidationErrorsProps> = ({ error }) => {
   if (!error) return null;
 
-  console.log('ValidationErrors error: ', error);
+  const renderAlert = (props: AlertProps) => {
+    return <Alert className="sha-validation-error-alert" type="error" showIcon {...props} />;
+  };
 
   let errorObj = error as IErrorInfo;
 
@@ -34,11 +30,7 @@ export const ValidationErrors: FC<IValidationErrorsProps> = ({ error }) => {
     errorObj = error['data']['error'] as IErrorInfo;
   }
 
-  const { code, message, details, validationErrors } = errorObj;
-
-  if (!code && !message && !details && !validationErrors) {
-    return renderAlert({ message: DEFAULT_ERROR_MSG });
-  }
+  const { message, details, validationErrors } = errorObj;
 
   if (validationErrors?.length) {
     const violations = (
@@ -53,11 +45,11 @@ export const ValidationErrors: FC<IValidationErrorsProps> = ({ error }) => {
   }
 
   if (message) {
-    // return renderAlert({ message });
+    return renderAlert({ message });
   }
 
   if (details) {
-    // return renderAlert({ message: details });
+    return renderAlert({ message: details });
   }
 
   return renderAlert({ message: DEFAULT_ERROR_MSG });
