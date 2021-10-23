813b535a7dfd13bd0daf6ed3329dd26744ed78f3
Phil Mashiane <phillemonm@boxfusion.co.za>
2021-10-23 16:41:08 +0200

    Rearranged deps and peer deps to reduce library size

--- src/components/formDesigner/toolboxComponent.tsx
+++ src/components/formDesigner/toolboxComponent.tsx
@@ -1,15 +1,10 @@
 import React, { FC } from 'react';
 import { IToolboxComponentBase } from '../../interfaces';
-import styled from 'styled-components';
 
 export interface IProps {
   component: IToolboxComponentBase;
   index: number;
 }
-
-const Item = styled.div`
-  border: 1px ${props => (props.isDragging ? 'dashed #4099ff' : 'solid #ddd')};
-`;
 
 const ToolbarComponent: FC<IProps> = ({ component /*, index*/ }) => {
   const ComponentContent = () => (
@@ -20,9 +15,9 @@ const ToolbarComponent: FC<IProps> = ({ component /*, index*/ }) => {
   );
 
   return (
-    <Item className="sha-toolbox-component">
+    <div className="sha-toolbox-component">
       <ComponentContent />
-    </Item>
+    </div>
   );
 };
 
