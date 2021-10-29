90adbfe38940532242268b461fa0d63082900daa
AlexanderSlavchov <83951884+AlexanderSlavchov@users.noreply.github.com>
2021-10-29 12:22:44 +0200

    MainLayout in Storybook is not showing fix

--- src/components/mainLayout/index.stories.tsx
+++ src/components/mainLayout/index.stories.tsx
@@ -7,8 +7,9 @@ import MainLayout, { IMainLayoutProps } from './';
 import { SIDEBAR_MENU_ITEMS } from './menuItems';
 
 export default {
-  title: 'Components/Layout',
+  title: 'Components/MainLayout',
   component: MainLayout,
+  argTypes: {},
 } as Meta;
 
 const defaultProps: IMainLayoutProps = {
@@ -19,17 +20,19 @@ const defaultProps: IMainLayoutProps = {
 const backendUrl = process.env.STORYBOOK_BASE_URL; // TODO: Make this configurable
 
 // Create a master template for mapping args to render the Button component
-const Template: Story<IMainLayoutProps> = args => (
-  <ShaApplicationProvider backendUrl={backendUrl}>
-    <AuthContainer layout={true}>
-      <SidebarMenuDefaultsProvider items={SIDEBAR_MENU_ITEMS}>
-        <MainLayout {...args} title="Any title">
-          <div>This is a div</div>
-        </MainLayout>
-      </SidebarMenuDefaultsProvider>
-    </AuthContainer>
-  </ShaApplicationProvider>
-);
+const Template: Story<IMainLayoutProps> = args => {
+  return (
+    <ShaApplicationProvider backendUrl={backendUrl}>
+      <AuthContainer layout={true}>
+        <SidebarMenuDefaultsProvider items={SIDEBAR_MENU_ITEMS}>
+          <MainLayout {...args} title="Any title">
+            <div>This is a div</div>
+          </MainLayout>
+        </SidebarMenuDefaultsProvider>
+      </AuthContainer>
+    </ShaApplicationProvider>
+  );
+};
 
 export const Default = Template.bind({});
 
