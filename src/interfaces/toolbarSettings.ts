7b56887a9f37c9a07c6117836b2c5e814cd79084
Phil Mashiane <phillemonm@boxfusion.co.za>
2021-11-10 10:54:13 +0200

    Minor refactoring on the DesignerToolbarSettings

--- src/interfaces/toolbarSettings.ts
+++ src/interfaces/toolbarSettings.ts
@@ -33,104 +33,81 @@ type LabelValueEditorType = ToolbarSettingsProp & Omit<ILabelValueEditorProps, '
 type QueryBuilderType = ToolbarSettingsProp & Omit<IQueryBuilderProps, 'type'>;
 
 export class DesignerToolbarSettings {
-  private settings: IConfigurableFormComponent[];
+  private form: IConfigurableFormComponent[];
 
   constructor() {
-    this.settings = [];
+    this.form = [];
   }
 
   addDropdown(props: DropdownType) {
-    this.settings.push({ ...props, type: 'dropdown' });
+    this.form.push({ ...props, type: 'dropdown' });
 
     return this;
   }
 
   addSectionSeparator(props: SectionSeparatorType) {
-    this.settings.push({ ...props, type: 'sectionSeparator' });
+    this.form.push({ ...props, type: 'sectionSeparator' });
 
     return this;
   }
 
   addTextField(props: TextFieldType) {
-    this.settings.push({ ...props, type: 'textField' });
+    this.form.push({ ...props, type: 'textField' });
 
     return this;
   }
 
   addTextArea(props: TextAreaType) {
-    this.settings.push({ ...props, type: 'textArea' });
+    this.form.push({ ...props, type: 'textArea' });
 
     return this;
   }
 
   addIconPicker(props: IconPickerType) {
-    this.settings.push({ ...props, type: 'iconPicker' });
+    this.form.push({ ...props, type: 'iconPicker' });
 
     return this;
   }
 
   addAutocomplete(props: AutocompleteType) {
-    this.settings.push({ ...props, type: 'autocomplete' });
+    this.form.push({ ...props, type: 'autocomplete' });
 
     return this;
   }
 
   addCheckbox(props: CheckboxType) {
-    this.settings.push({ ...props, type: 'checkbox' });
+    this.form.push({ ...props, type: 'checkbox' });
 
     return this;
   }
 
   addNumberField(props: NumberFieldType) {
-    this.settings.push({ ...props, type: 'numberField' });
+    this.form.push({ ...props, type: 'numberField' });
 
     return this;
   }
 
   addLabelValueEditor(props: LabelValueEditorType) {
-    this.settings.push({ ...props, type: 'labelValueEditor' });
+    this.form.push({ ...props, type: 'labelValueEditor' });
 
     return this;
   }
 
   addQueryBuilder(props: QueryBuilderType) {
-    this.settings.push({ ...props, type: 'queryBuilder' });
+    this.form.push({ ...props, type: 'queryBuilder' });
 
     return this;
   }
 
+  get settings() {
+    return this.form;
+  }
+
   toJson() {
-    return this.settings;
+    return this.form;
   }
 
   toJsonString() {
-    // reat;
-    return JSON?.stringify(this.settings);
+    return JSON?.stringify(this.form);
   }
 }
-
-const settings = new DesignerToolbarSettings();
-
-settings
-  .addDropdown({ id: 'dce9c4fa-4190-11ec-81d3-0242ac130003', name: 'someName', dataSourceType: 'entityList' })
-  .addSectionSeparator({ id: 'dce9c7ca-4190-11ec-81d3-0242ac130003', name: 'someName', sectionName: 'someSection' })
-  .addTextField({ id: 'dce9c89c-4190-11ec-81d3-0242ac130003', name: 'someTextField' })
-  .addIconPicker({ id: 'dce9c95a-4190-11ec-81d3-0242ac130003', name: 'someIconPicker' })
-  .addAutocomplete({
-    id: 'dce9c95a-4190-11ec-81d3-0242ac130003',
-    name: 'someIconPicker',
-    entityTypeShortAlias: { id: 'dce9c4fa-4190-11ec-81d3-0242ac130003' },
-    dataSourceType: 'entitiesList',
-  })
-  .addCheckbox({ id: 'dce9ca0e-4190-11ec-81d3-0242ac130003', name: 'checkboxName' })
-  .addNumberField({ id: 'dce9cac2-4190-11ec-81d3-0242ac130003', name: 'someNumberField' })
-  .addLabelValueEditor({
-    id: 'dce9cb76-4190-11ec-81d3-0242ac130003',
-    name: 'someLabelValue',
-    labelName: 'labelName',
-    labelTitle: 'labelTitle',
-    valueName: 'valueName',
-    valueTitle: 'valueTitle',
-  })
-  .addQueryBuilder({ id: 'dce9cb76-4190-11ec-81d3-0242ac130003', name: 'queryBuilder' })
-  .toJson();
