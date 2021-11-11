872610ffe4e20288f061619f622bc0aec4a7041d
Phil Mashiane <phillemonm@boxfusion.co.za>
2021-11-11 21:45:23 +0200

    Removed unused import

--- src/components/appConfigurator/configurableComponent.tsx
+++ src/components/appConfigurator/configurableComponent.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo, useState } from 'react';
+import React, { FC, useState } from 'react';
 import { useAppConfigurator /*, useConfigurableComponent*/ } from '../../providers';
 import { ComponentSettingsModal } from './componentSettingsModal';
 
