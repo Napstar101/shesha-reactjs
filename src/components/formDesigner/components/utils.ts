2c9b35155cab28063fd2e7f0ca3594bf38b4f88e
Luke <Lukeybooi@gmail.com>
2022-02-23 20:41:41 +0200

    Removed settings from onCustomEventsHandler

--- src/components/formDesigner/components/utils.ts
+++ src/components/formDesigner/components/utils.ts
@@ -7,15 +7,15 @@ export const onCustomEventsHandler = <FormCustomEvent = any>(
   customEventAction: string,
   form: FormInstance
 ) => {
-  /* tslint:disable:function-constructor */  
-  const eventFunc = new Function('event', 'form', 'settings', customEventAction);
+  /* tslint:disable:function-constructor */
+  const eventFunc = new Function('event', 'form', customEventAction);
 
   return eventFunc(event, form);
 };
 
 export const customEventHandler = <T = any>(
   model: IConfigurableFormComponent,
-  form: FormInstance,
+  form: FormInstance
 ): DOMAttributes<T> => ({
   onBlur: event => onCustomEventsHandler(event, model?.onBlurCustom, form),
   onChange: event => onCustomEventsHandler(event, model?.onChangeCustom, form),
