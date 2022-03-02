f4de4c881da44e802b4c370083ed4e3527eaf22c
Phil <phillymza@gmail.com>
2022-03-02 21:21:47 +0200

    fix: issue cause by onSelectedIdsChanged not being defined in the ReactTable component

--- src/components/reactTable/index.tsx
+++ src/components/reactTable/index.tsx
@@ -146,7 +146,7 @@ const ReactTable: FC<IReactTableProps> = ({
   const { pageIndex, pageSize, selectedRowIds } = state;
 
   useEffect(() => {
-    if (selectedRowIds) {
+    if (selectedRowIds && typeof onSelectedIdsChanged === 'function') {
       const arrays: string[] = data
         ?.map(({ Id }, index) => {
           if (selectedRowIds[index]) {
@@ -159,7 +159,7 @@ const ReactTable: FC<IReactTableProps> = ({
 
       onSelectedIdsChanged(arrays);
     }
-  }, [selectedRowIds]);
+  }, [selectedRowIds, onSelectedIdsChanged]);
 
   // Listen for changes in pagination and use the state to fetch our new data
   useEffect(() => {
