ec7aa5b318ade9943cc90e280d11799cb5c14484
Phil <phillymza@gmail.com>
2022-03-02 21:15:56 +0200

    wip: create child table control

--- src/components/formDesigner/components/dataTable/childTable/index.tsx
+++ src/components/formDesigner/components/dataTable/childTable/index.tsx
@@ -1,6 +1,7 @@
 import { TableOutlined } from '@ant-design/icons';
 import { useForm } from 'antd/lib/form/Form';
 import React from 'react';
+import { CollapsiblePanel } from '../../../..';
 import { validateConfigurableComponentSettings } from '../../../../../formDesignerUtils';
 import { IConfigurableFormComponent, IToolboxComponent } from '../../../../../interfaces';
 import { FormMarkup } from '../../../../../providers/form/models';
@@ -18,7 +19,17 @@ const ChildTableComponent: IToolboxComponent<IChildTableComponentProps> = {
   factory: (model: IChildTableComponentProps) => {
     const {} = useForm();
 
-    return <ComponentsContainer containerId={model.id} />;
+    console.log('ChildTableComponent model.id :>> ', model.id);
+
+    return (
+      <CollapsiblePanel
+        key={undefined}
+        header={''}
+        extra={<ComponentsContainer containerId={'93053321-71c5-4d96-9832-d860cac70761'} />}
+      >
+        <ComponentsContainer containerId={model.id} />
+      </CollapsiblePanel>
+    );
   },
   settingsFormMarkup: settingsForm,
   validateSettings: model => validateConfigurableComponentSettings(settingsForm, model),
